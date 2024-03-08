8cfc82d979634c077e9f58416732448ea6d1d445
Chris Shearer <cbshearer@gmail.com>
2024-03-08 14:25:23 -0800

    Update mg_send.py

--- mg_send.py
+++ mg_send.py
@@ -3,25 +3,18 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-## your configured mailgun domain
-mg_domain = "mg.example.com"
-## username is always API (https://documentation.mailgun.com/docs/mailgun/api-reference/authentication/)
-mg_user = "api"
-## API key (menu > API Security > Mailgun API Keys)
-mg_pass = "EXAMPLE-KEY-DATA"
-## address the message will be FROM
-mg_from = "sender@example.com"
-## address the message will be TO
-mg_to = "recipient@example.com"
+mg_domain = "mg.example.com"        ## your configured mailgun domain
+mg_user = "api"                     ## username is always API (https://documentation.mailgun.com/docs/mailgun/api-reference/authentication/)
+mg_pass = "EXAMPLE-KEY-DATA"        ## API key (menu > API Security > Mailgun API Keys)
 
-send_msg =  requests.post(
-f"https://api.mailgun.net/v3/{mg_domain}/messages",
-auth=(f"{mg_user}", f"{mg_pass}"),
-data={"from": f"{mg_from}",
-    "to": f"{mg_to}",
-    "subject": "Hello there sir",
-    "text": "Testing 1-2-3-4"},
-    verify=False)
+mg_sender = "sender@example.com"
+mg_recipient = "recipient@example.com"
+mg_subject = "test subject"
+mg_message = "this is a test body!"
+
+send_url = f"https://api.mailgun.net/v3/{mg_domain}/messages"
+send_data = {"from": mg_sender, "to": mg_recipient, "subject": mg_subject, "text": mg_message}
+send_msg =  requests.post(send_url, auth=(mg_user, mg_pass), data={"from": mg_sender, "to": mg_recipient, "subject": mg_subject, "text": mg_message}, verify=False)
 
 print(send_msg.status_code)
 print(send_msg.text)
